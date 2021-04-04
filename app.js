e34bd764c4660c67b6ed90a2f3ca93767ac6b6a7
= <=>
2021-04-04 11:24:36 -0300

    addig sequelize

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
 const productRouters = require('./routes/products');
+const morgan = require('morgan');
 
+app.use(morgan('dev'));
 app.use('/product', productRouters);
+
+app.use((request, response, next) => {
+    const error = new Error('Page not found');
+    error.status = 404;
+    next(error);
+})
 
 module.exports = app;
